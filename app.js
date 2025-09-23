60d3be740250912dfc6e263b14d136a2c412e9c3
nikolajpirum <nikolajpirum.10@gmail.com>
2025-09-23 22:29:19 +0200

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/holiday",(req,res) => {
+    res.sendFile(__dirname + "/static/index.html");
+});
+
+            
+const PORT = 8080;
+app.listen(PORT,() =>
+console.log('Server is running on port',PORT)
+)
