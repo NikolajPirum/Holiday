15756a5c7c7bd1b19b3ad8dcd1b1472f31f7e976
nikolajpirum <nikolajpirum.10@gmail.com>
2025-09-24 21:58:56 +0200

    added countdown for holiday

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const app = express();
 app.get("/holiday",(req,res) => {
     res.sendFile(__dirname + "/static/index.html");
 });
+       
+    const currentTime = new Date().toLocaleTimeString("un-ek")
+    console.log(currentTime)  
+    const nodeJsObj = { name: "Braingymnastics with with Anders", date : new Date("2025-09-25")};
+    console.log(nodeJsObj.date.getTime())
+    const now = new Date().getTime();
+    console.log(now)
 
-            
+
 const PORT = 8080;
 app.listen(PORT,() =>
 console.log('Server is running on port',PORT)
